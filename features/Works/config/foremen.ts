e559e0de1204e3020189930e73eb0515b92407a9
Stanislav <stanislav.novikov.work.71@gmail.com>
2026-05-07 09:57:07 +0300

    add new prorab

--- features/Works/config/foremen.ts
+++ features/Works/config/foremen.ts
@@ -13,29 +13,41 @@ export interface Foreman {
  * Список прорабов для использования в системе заказов
  */
 export const foremenConfig: Foreman[] = [
-  {
+   {
     id: 1,
-    name: "Ломакин Александр",
-    firstName: "Александр",
-    lastName: "Ломакин",
+    name: "Абрамов Алексей",
+    firstName: "Алексей",
+    lastName: "Абрамов",
   },
-  {
+    {
     id: 2,
-    name: "Гулин Георгий",
-    firstName: "Георгий",
-    lastName: "Гулин",
-  },
-  {
-    id: 3,
     name: "Липаткин Максим",
     firstName: "Максим",
     lastName: "Липаткин",
   },
   {
+    id: 3,
+    name: "Гулин Георгий",
+    firstName: "Георгий",
+    lastName: "Гулин",
+  },
+  {
     id: 4,
-    name: "Абрамов Алексей",
-    firstName: "Алексей",
-    lastName: "Абрамов",
+    name: "Ломакин Александр",
+    firstName: "Александр",
+    lastName: "Ломакин",
+  },
+  {
+    id: 5,
+    name: "Орлов Максим",
+    firstName: "Максим",
+    lastName: "Орлов",
+  },
+    {
+    id: 6,
+    name: "Добросол Андрей",
+    firstName: "Андрей",
+    lastName: "Добросол",
   },
 ];
 
