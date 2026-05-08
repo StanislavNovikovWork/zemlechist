2567e92d5f8aee6046b602c263dd5b709d011bbb
Stanislav <stanislav.novikov.work.71@gmail.com>
2026-05-08 08:40:46 +0300

    fix rangePicker

--- features/SupplierDrawerControll/ui/SupplierForm/SupplierForm.tsx
+++ features/SupplierDrawerControll/ui/SupplierForm/SupplierForm.tsx
@@ -1,6 +1,8 @@
 'use client';
 
-import { Form, Input, Button, Space, Select, DatePicker, Rate } from 'antd';
+import { Form, Input, Button, Space, Select, DatePicker, Rate, ConfigProvider } from 'antd';
+import ruRU from 'antd/locale/ru_RU';
+const { RangePicker } = DatePicker;
 import { PhoneInput } from '@/ui/PhoneInput';
 import { useEffect, useMemo } from 'react';
 import dayjs from 'dayjs';
@@ -36,6 +38,7 @@ export function AddSupplierForm({
   }, [form]);
 
   useEffect(() => {
+    console.log(initialValues)
     if (!initialValues) return;
 
     const values: any = { ...initialValues };
@@ -43,6 +46,15 @@ export function AddSupplierForm({
     if (values.updatedAt) {
       const parsed = dayjs(values.updatedAt, 'DD.MM.YYYY');
       values.updatedAt = parsed.isValid() ? parsed : undefined;
+    }
+
+    if (values.duration?.period1) {
+      const startDate = dayjs(values.duration.period1[0], 'DD.MM.YYYY');
+      const endDate = dayjs(values.duration.period1[1], 'DD.MM.YYYY');
+      values.duration = [
+        startDate.isValid() ? startDate : null,
+        endDate.isValid() ? endDate : null,
+      ];
     }
 
     if (values.coordinates && Array.isArray(values.coordinates)) {
@@ -77,12 +89,7 @@ export function AddSupplierForm({
         return <DatePicker style={{ width: '100%' }} format="DD.MM.YYYY" />;
 
       case 'dateRange':
-        return (
-          <div style={{ display: 'flex', gap: '8px' }}>
-            <DatePicker style={{ width: '50%' }} format="DD.MM.YYYY" placeholder="Начало" />
-            <DatePicker style={{ width: '50%' }} format="DD.MM.YYYY" placeholder="Конец" />
-          </div>
-        );
+        return <RangePicker style={{ width: '100%' }} format="DD.MM.YYYY" />;
 
       case 'rate':
         return <Rate />;
@@ -115,14 +122,16 @@ export function AddSupplierForm({
   return (
     <div className="flex flex-col h-full">
       <div className="flex-1 overflow-y-auto px-4">
-        <Form
-          form={form}
-          layout="vertical"
-          onFinish={handleFinish}
-          className="px-4"
-        >
-          {renderFormItems({ schema: currentSchema })}
-        </Form>
+        <ConfigProvider locale={ruRU}>
+          <Form
+            form={form}
+            layout="vertical"
+            onFinish={handleFinish}
+            className="px-4"
+          >
+            {renderFormItems({ schema: currentSchema })}
+          </Form>
+        </ConfigProvider>
       </div>
       <div className="pt-4 px-4 bg-inherit w-full">
         <Space className="w-full justify-end">
